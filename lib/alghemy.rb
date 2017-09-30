0cd109c42bf24c4c153da4bdc927c7b80a3981b0
jc_morph <jc_morph@caramail.com>
2017-09-30 21:28:22 +0100

    tidy

--- lib/alghemy.rb
+++ lib/alghemy.rb
@@ -5,37 +5,33 @@ module Alghemy
   require_relative 'alghemy/affinities'
 end
 
-# Element.class GLOSSARY
-
-# ACCESSIBLE ATTRIBUTES
-
-# @sijil = the abstracted digital location of your element ( "path/file.ext" )
-
-# Properties
-# All elements have properties.
-# The innate properties are different for each type of element.
+# Matter GLOSSARY
+#
+# Aspects
+# All elements have aspects.
+# The innate aspects are different for each type of element.
 
 # ---
 # Image and Video
-# @space = spacial dimension ( "horizontal x vertical" )
+# space    = spacial dimension ("horizontal x vertical")
 
 # ---
 # Video and Sound
-# @time = temporal dimension ( duration )
+# time     = temporal dimension (Float)
 
 # Because Video and Sound elements are perceivable in the temporal dimension,
 #  they necessarily possess two other related properties:
 
-# @frequency = rate ( Video.( framerate ) || Sound.( samplerate ) )
-# @lifespan  = total ( Video.( frames )    || Sound.( samples ) )
+# freq     = rate (Video:framerate || Sound:samplerate)
+# lifespan = total   (Video:frames || Sound:samples)
 
 # ---
 # Sound and Image
-# @depth = bit depth ( depth )
+# depth    = bitdepth (Integer)
 
 # ---
 # All Elements
-# @arcana = ( Video.( pixel_format ) || Sound.( encoding ) || Image.( colorspace )
+# arcana   = (Video:pixel_format || Sound:encoding || Image:colorspace)
 
-# @mems = an Array retaining information of all non-mutagenic transmutations
-#  undertaken between an Element's initial and current states.
+# @mems    = Array retaining information of all transmutations
+#  undertaken between Matter's initial and current states.
