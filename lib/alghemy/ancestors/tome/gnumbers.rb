eabd1a6d879959ff91b10f90936be6ff147838a3
JC Morph <jc_morph@caramail.com>
2021-03-27 00:13:06 +0000

    handle filenames with empty dirs properly when replacing glob patterns

--- lib/alghemy/ancestors/tome/gnumbers.rb
+++ lib/alghemy/ancestors/tome/gnumbers.rb
@@ -14,11 +14,22 @@ module Alghemy
     # Public: Replace gnums in String with a suitable glob pattern.
     def glob_replace( string, numbers )
       ranges = e_ranges(string, numbers)
-      string = string.dup
       ranges.reverse_each.with_object(string.dup) do |rng, str|
         glob = rng.size > 2 ? '*' : ('?' * rng.size)
         str[rng] = glob
       end
+    end
+
+    # Public: Returns Array of ranges for locations of gnums in String.
+    def e_ranges( string, numbers )
+      ranges = []
+      e_nums(numbers).each.with_index do |enum, i|
+        start  = num_index(string)[i]
+        finish = start + enum.size - 1
+        ranges << (start..finish)
+      end
+      binding.pry
+      ranges
     end
 
     # Public: Returns Array of numbers in num_list that iterate.
@@ -28,28 +39,16 @@ module Alghemy
       num_list.transpose.collect do |row|
         enum = row.uniq.size.between?( 2, row.size )
         enum ? row.first : nil
-      end
+      end.compact
     end
 
     private
 
-    # Public: Returns Array of ranges for locations of gnums in String.
-    def e_ranges( string, numbers )
-      ranges = []
-      e_nums(numbers).each.with_index do |enum, i|
-        if enum
-          start  = num_index(string)[i]
-          finish = start + enum.size
-          ranges << (start..finish - 1)
-        end
-      end
-      ranges
-    end
-
     # Internal: Returns Array of indices for numbers in String.
     def num_index( string )
       arr = []
-      pad = File.dirname(string).size.succ
+      dir = File.dirname string
+      pad = dir == '.' ? 0 : dir.size.succ
       File.basename(string).scan(/\d+/) do
         index = Regexp.last_match.offset(0)[0]
         arr << index + pad
