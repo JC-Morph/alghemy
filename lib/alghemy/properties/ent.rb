8cec6fd7d6660f7525d14e9121916d07d1be7914
JC-Morph <jc_morph@caramail.com>
2022-01-21 18:26:51 +0000

    Clarity update: syntax

--- lib/alghemy/properties/ent.rb
+++ lib/alghemy/properties/ent.rb
@@ -12,11 +12,11 @@ module Alghemy
         select {|lmnt| lmnt.respond_to? :each }.size
       end
 
-      # Public: Ensures self is two dimensional.
+      # Public: Ensures value is a two dimensional Array.
       def balance( ent = nil, act = :unshift )
-        replace [flatten] if enumerable_size.zero?
-        send(act, ent)    if ent && size == 1
-        collect! {|ent_part| [ent_part].flatten } if size != enumerable_size
+        replace([flatten]) if enumerable_size.zero?
+        send(act, ent)     if ent && size == 1
+        map! {|ent_part| [ent_part].flatten } if size != enumerable_size
       end
 
       # Public: Ensures that bit is in a valid range for enc.
@@ -26,8 +26,8 @@ module Alghemy
         each do |ent|
           enc = encs.index {|check| ent[0][/^#{check}/] }
           bit = bits[enc]
-          chk = ent[1] && ent[1].to_i >= bit
-          ent[1] = bit unless chk && enc != 0
+          valid  = ent[1] && ent[1].to_i >= bit
+          ent[1] = bit unless valid && enc != 0
         end
         self
       end
@@ -35,7 +35,7 @@ module Alghemy
       def separate
         hsh = {}
         %i[enc bit].each.with_index do |ananym, i|
-          hsh[ananym] = size == 1 ? first.slice(i) : collect {|ent| ent[i] }
+          hsh[ananym] = size == 1 ? first.slice(i) : map {|ent| ent[i] }
         end
         hsh
       end
