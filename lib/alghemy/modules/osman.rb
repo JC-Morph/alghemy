b8b044f88618a517157c540c285d6787d4798784
JC-Morph <jc_morph@caramail.com>
2022-03-27 17:39:17 +0100

    Refactor Osman, allowing for more granular control

--- lib/alghemy/modules/osman.rb
+++ lib/alghemy/modules/osman.rb
@@ -4,22 +4,26 @@ module Alghemy
   module Modules
     # Public: Writes Rubrics for transmutations.
     module Osman
-      # Public: Method that builds command line to be executed.  Can be ducked
-      # by transmutations to allow more control.
+      # Public: Method that builds command line to be executed. May be ducked by
+      # transmutations.
       def write_rubric
         write.send stuff[:name]
       end
 
       def write( moniker = nil )
-        stuff = self.stuff.merge(is_raw: lmnt.raw?)
-        return rubric.write(stuff) unless moniker
-        rubric.new(moniker, stuff)
+        stuff    = self.stuff.merge(is_raw: lmnt.raw?)
+        rubric   = self.rubric.write(stuff) unless moniker
+        rubric ||= self.rubric.new(moniker, stuff)
+
       end
 
       # Public: Returns appropritate Rubric class for current transmutation.
       def rubric
-        return lmnt.class.rubric unless stuff[:affinity]
-        Affinities[stuff[:affinity]].rubric
+        rubric = stuff[:rubric]
+        return rubric if rubric
+        affinity = stuff[:affinity]
+        return lmnt.class.rubric unless affinity
+        Affinities[affinity].rubric
       end
 
       # Public: Duckable Hash of options to initiaise Rubric with.
