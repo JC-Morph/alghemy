01e71eae570c52fa11f7bf211051b665c4ee1818
JC-Morph <jc_morph@caramail.com>
2022-12-07 04:24:41 +0000

    Bugfix: fix variable reference

--- lib/alghemy/ancestors/tome/gnumbers.rb
+++ lib/alghemy/ancestors/tome/gnumbers.rb
@@ -1,3 +1,5 @@
+require 'alghemy/glyphs'
+
 module Alghemy
   # Public: Handles identifying iterative numbers in filename Strings.
   module Gnumbers
@@ -14,17 +16,18 @@ module Alghemy
     # Public: Replace gnums in filename with a suitable glob pattern.
     def glob_replace( filename, numbers )
       ranges = e_ranges(filename, numbers)
-      ranges.reverse_each.with_object(filename.to_s) do |rng, str|
+      new_name = ranges.reverse_each.with_object(filename.to_s) do |rng, str|
         glob = rng.size > 2 ? '*' : ('?' * rng.size)
         str[rng] = glob
       end
+      Glyphs[:sijil].compose new_name
     end
 
-    # Public: Returns Array of ranges for locations of gnums in filename.
+    # Public: Returns Array of index ranges for e_nums in filename.
     def e_ranges( filename, numbers )
       ranges = []
       e_nums(numbers).each.with_index do |enum, i|
-        start  = num_index(filename)[i]
+        start  = num_index(filename)[enum]
         finish = start + enum.size - 1
         ranges << (start..finish)
       end
@@ -36,7 +39,7 @@ module Alghemy
       sizes = num_list.collect(&:size).sort
       return [] if sizes.first != sizes.last
       num_list.transpose.collect do |row|
-        enum = row.uniq.size.between?( 2, row.size )
+        enum = row.uniq.size.between?(2, row.size)
         enum ? row.first : nil
       end.compact
     end
@@ -45,14 +48,15 @@ module Alghemy
 
     # Internal: Returns Array of indices for numbers in filename.
     def num_index( filename )
-      arr = []
+      hsh = {}
       dir = File.dirname filename.to_s
       pad = dir == '.' ? 0 : dir.size.succ
       File.basename(filename.to_s).scan(/\d+/) do
-        index = Regexp.last_match.offset(0)[0]
-        arr << index + pad
+        match = Regexp.last_match
+        index = match.offset(0)[0]
+        hsh[match.to_s] = index + pad
       end
-      arr
+      hsh
     end
   end
 end
