aa4f6ae15b5757eba0c4cd8a42dffbceeed63f9c
JC-Morph <jc_morph@caramail.com>
2023-01-27 20:42:32 +0000

    Clarity update: correct argument reference in doc

--- lib/alghemy/methods/store.rb
+++ lib/alghemy/methods/store.rb
@@ -6,7 +6,7 @@ module Alghemy
     module Store
       # Public: Stores an entry in the archive as a Memory.
       #
-      # memory - Hash of options from a transform.
+      # entry - Hash of options from a transform.
       def store( entry )
         memory = Glyphs[:memory].new(entry)
         Glyphs[:archive].store(self.to_s, memory)
