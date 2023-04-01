0263c95aa933736164a4b74bc2b84201dc20707f
JC-Morph <jc_morph@caramail.com>
2023-04-01 16:30:48 +0100

    Refactor DefineTransforms

--- lib/alghemy/meta/define_transforms.rb
+++ lib/alghemy/meta/define_transforms.rb
@@ -5,21 +5,25 @@ module Alghemy
     # Public: Enables Transmutations for an object. Defines individual
     # transformations on object as named methods.
     module DefineTransforms
+      private
+
       # Internal: Method for defining Transforms on Matter. Writes files.
       #
       # Returns new Matter, dependent on Transform.
       def define_transforms( bandoleer )
         bandoleer.equipped.each do |transform|
-          define_method transform do |*priorities, **stuff|
-            tran     = bandoleer[__callee__]
-            expected = tran.expects
-            lmnt = self
-            until expected.include?(lmnt.affinity)
-              lmnt = mould(expected, lmnt)
-            end
-            tran = tran.new(lmnt, *priorities, **stuff)
-            Comrades[:alghemist].transmute(lmnt, tran)
-          end
+          define_transform transform
+        end
+      end
+
+      def define_transform( transform )
+        define_method transform do |*priorities, **stuff|
+          tran = bandoleer[__callee__]
+          expected = tran.expects
+          lmnt = self
+          lmnt = mould(expected, lmnt) until expected.include? lmnt.affinity
+          tran = tran.new(lmnt, *priorities, **stuff)
+          Comrades[:alghemist].transmute(lmnt, tran)
         end
       end
     end
