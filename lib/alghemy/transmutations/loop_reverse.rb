69a4a4620c38a9d43ae73a6672a382ec8e0ac35b
JC-Morph <jc_morph@caramail.com>
2023-04-21 23:28:03 +0100

    Make LoopReverse a perfect loop

--- lib/alghemy/transmutations/loop_reverse.rb
+++ lib/alghemy/transmutations/loop_reverse.rb
@@ -13,17 +13,21 @@ module Alghemy
       end
 
       def write_rubric( rubric = nil )
-        rubric = write(rubric).input
+        rubric  = write(rubric).input
+        rate    = stuff[:rate]
+        seg_end = stuff[:span] - 1
         rubric.add("-filter_complex '
-                   [0]reverse[r];[0][r]concat,
-                   setpts=N/#{stuff[:rate]}/TB'")
+                   [0]segment=frames=1|#{seg_end}[s1][s2][s3];
+                   [s1][s3]concat,nullsink;
+                   [s2]reverse[r];
+                   [0][r]concat,setpts=N/#{rate}/TB'")
         rubric.output
       end
 
       private
 
       def defaults
-        {ext: 'mp4', label: 'Lr', rate: lmnt.rate}
+        {ext: 'mp4', label: 'Lr', rate: lmnt.rate, span: lmnt.span}
       end
     end
   end
