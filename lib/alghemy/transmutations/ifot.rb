cfa1c71965e1774c27b9a2ec1b7dab6980a21e5f
JC-Morph <jc_morph@caramail.com>
2023-04-24 23:55:35 +0100

    Refactor Ifot Transmutation

--- lib/alghemy/transmutations/ifot.rb
+++ lib/alghemy/transmutations/ifot.rb
@@ -12,20 +12,24 @@ module Alghemy
       def tran_init
         stuff[:enum] = :group
         @tome = sub_tome if stuff[:magni] || stuff[:phase]
-        @mult = false if @tome.size == 2
+        @mult = false if tome.size == 2
       end
 
       private
 
-      # Public: Define Tome (list of inputs) to be used. Parses inputs, which
-      # may include explicit :magnitude or :phase images.
+      # Internal: Define Tome (list of inputs) to be used.
+      # Parses inputs, which may include explicit :magnitude or :phase images.
       def sub_tome
-        tome = %i[magni phase].collect do |part|
-          part = stuff[part] || stuff[:tome]
-          part.is_a?(Array) ? part : part.list
-        end.transpose
+        tome = collect_components
         tome.flatten! if tome.size == 1
         list tome
+      end
+
+      def collect_components
+        %i[magni phase].map do |cmpnt|
+          cmpnt = stuff[cmpnt] || stuff[:tome]
+          cmpnt.is_a?(Array) ? cmpnt : cmpnt.list
+        end.transpose
       end
     end
   end
