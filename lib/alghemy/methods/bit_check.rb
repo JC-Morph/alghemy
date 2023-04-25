fec030fbf874fdc9e84502d08bd24c9c3804644c
JC-Morph <jc_morph@caramail.com>
2023-04-25 22:19:54 +0100

    Refactor BitCheck Method

--- lib/alghemy/methods/bit_check.rb
+++ lib/alghemy/methods/bit_check.rb
@@ -1,20 +1,31 @@
 module Alghemy
   module Methods
+    # Public: Method for validating bitdepth values for types of audio encoding.
     module BitCheck
+      # Public: Checks whether a given bit value is valid for a given audio
+      # encoding type. Possible types are: a-law, u-law, signed, unsigned,
+      # and float. If out of range, returns the closest value that isn't.
+      #
+      # Returns Integer.
       def bit_check( enc, val )
-        bit_range = depth_limits.find {|type, _| enc[/#{type}/] }.last
-        valid = bit_range.include? val.to_i
-        return val if valid
-        val > bit_range.last ? bit_range.last : bit_range.first
+        bit_range = find_bit_range enc
+        min, max  = bit_range.minmax
+        val = val.to_i
+        return val if bit_range.include?(val)
+        val < min ? min : max
       end
 
       private
 
+      def find_bit_range( enc )
+        depth_limits.find {|type, _| enc[/#{type}/] }.last
+      end
+
       def depth_limits
         {
-          "^a|u" => 8..8,
-          "^s"   => 16..32,
-          "f"    => 32..64
+          "^a|u" => [8],
+          "^s"   => [16, 24, 32],
+          "f"    => [32, 48, 64]
         }
       end
     end
