a233cfcf0ae825f8877dea7157242d2826a3b99a
JC-Morph <jc_morph@caramail.com>
2023-04-30 14:50:41 +0100

    Enable automation for Mutate

--- lib/alghemy/transmutations/mutate.rb
+++ lib/alghemy/transmutations/mutate.rb
@@ -6,6 +6,9 @@ module Alghemy
   module Transmutations
     # Public: Process Sound with VST effect plugins.
     class Mutate < Ancestors[:transmutation]
+      Vst = Mutagens[:vst]
+      attr_reader :params
+
       def self.priorities
         [:plugin, :ext]
       end
@@ -18,18 +21,15 @@ module Alghemy
         Rubrics[:mrs]
       end
 
-      def vst
-        Mutagens[:vst]
-      end
-
       def tran_init
-        stuff[:plugin] = vst.new stuff[:plugin]
+        stuff[:plugin] = Vst.new stuff[:plugin]
+        @params = stuff[:plugin].params
         stuff[:option_templates] = option_templates
       end
 
       def write_rubric( rubric = nil )
-        vst = stuff[:plugin].sijil
-        rubric = write(rubric).input.plugin(vst)
+        rubric = write(rubric).input.plugin(stuff[:plugin].sijil)
+        add_params rubric
         rubric.automate if stuff[:data]
         rubric.output
       end
@@ -41,12 +41,11 @@ module Alghemy
           autotrim: true,
           label:    'V',
           ext:      lmnt.sijil.ext,
-          plugin:   vst.list.sample
+          plugin:   Vst.list.sample
         }
       end
 
       def option_templates
-        params = stuff[:plugin].params
         params.transform_values.with_index do |val, idx|
           {
             delim:   ',',
@@ -54,6 +53,12 @@ module Alghemy
             flag:    "parameter #{idx}",
             default: val
           }
+        end
+      end
+
+      def add_params( rubric )
+        params.keys.each do |param|
+          rubric.send(param) if stuff.keys.include?(param)
         end
       end
     end
