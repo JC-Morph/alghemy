2d02bf60cf49d2aaabbcd40d9a43abc6165b1b8d
JC-Morph <jc_morph@caramail.com>
2023-06-05 17:03:44 +0100

    Reverse Congeal Transmutation functionality

--- lib/alghemy/transmutations/congeal.rb
+++ lib/alghemy/transmutations/congeal.rb
@@ -5,24 +5,29 @@ module Alghemy
   module Transmutations
     class Congeal < Ancestors[:transmutation]
       def self.expects
-        ['.bmp']
+        with_plural :Sound
       end
 
       def rubric
         Rubrics[:arss]
       end
 
+      def tran_init
+        height = lmnt.inherit(:size)&.y
+        stuff[:height] ||= height || 500
+      end
+
       def write_rubric( rubric = nil )
         rubric = write(rubric).input
-        rubric.min.max.rate.pps.brightness
-        rubric.send(stuff[:noise] ? :noise : :sine)
+        rubric.min.max.height.pps
+        [:brightness, :linear].each {|opt| rubric.send(opt) if stuff[opt] }
         rubric.output
       end
 
       private
 
       def defaults
-        {ext: 'wav'}
+        {ext: 'bmp'}
       end
     end
   end
