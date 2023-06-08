4cfcef7efc5e65d0a2d34b1caf880d17a3dcc862
JC-Morph <jc_morph@caramail.com>
2023-06-08 19:22:35 +0100

    Add new revertable Transmutations to REVERTABLE

--- lib/config.rb
+++ lib/config.rb
@@ -70,6 +70,9 @@ module Alghemy
     ifot:      :ffot,
     frames:    :compile,
     compile:   :frames,
+    congeal:   :dissolve,
+    dissolve:  :congeal,
+    splitrgb:  :conjoin,
     sublimate: :sublimate
   }.freeze
 end
