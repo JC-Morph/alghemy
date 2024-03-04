e9b41e1c33149692979a00b6693458d50365e591
JC-Morph <jc_morph@caramail.com>
2024-03-04 19:57:41 +0000

    Hide warnings when using VideoOpen to play videos

--- lib/alghemy/requests/video_open.rb
+++ lib/alghemy/requests/video_open.rb
@@ -7,7 +7,7 @@ module Alghemy
       extend Modules[:request]
 
       def self.moniker
-        %w[ffplay -hide_banner -loop 0]
+        %w[ffplay -hide_banner -loglevel error -loop 0]
       end
     end
   end
